85396a74a340e373a1860f267fc2d5f392acb970
Eduardo Zaqueu <zaq1@MacBook-Air-de-Eduardo.local>
2022-11-17 22:00:06 -0300

    final update

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,40 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
+mobileMenu = () => {
+  const menuItems = document.querySelector("nav ul");
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
-
-function menuAbrir() {
-  let menuAbrir = document.querySelector("nav ul");
-
-  if (menuAbrir.classList.contains("open")) {
-    menuAbrir.classList.remove("open");
+  if (menuItems.classList.contains("open")) {
+    menuItems.classList.remove("open");
     document.querySelector("#hide-me").classList.toggle("hide");
-    document.querySelector("#closebtn").classList.toggle("hide");
+    document.querySelector("#close-btn").classList.toggle("hide");
   } else {
-    menuAbrir.classList.add("open");
+    menuItems.classList.add("open");
     document.querySelector("#hide-me").classList.toggle("hide");
-    document.querySelector("#closebtn").classList.toggle("hide");
+    document.querySelector("#close-btn").classList.toggle("hide");
   }
 }
 
-let btn2 = document.querySelectorAll(".btn2");
+goToForms = () => {
+  const formsButton = document.querySelector(".forms-button");
 
-function btnaS() {
-  if (btn2) {
+  if (formsButton) {
     window.location = "https://docs.google.com/forms/d/e/1FAIpQLSfMLbPIRei9ry6l0EXnhObgoIaritElD8eHqFbcQvfuU9Of1Q/viewform";
     target = "_blank";
   }
 }
 
+smoothScrolling = () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+    } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll(".hidden"); 
+  hiddenElements.forEach((e) => observer.observe(e));
+}
+
+smoothScrolling();
 Modal.initElements();
